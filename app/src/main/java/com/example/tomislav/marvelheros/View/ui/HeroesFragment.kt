b3bbc5e77ae6209e491180ffa478066350ce4606
Tomislav <tomislav.lipovac@gmail.com>
2018-02-28 15:14:45 +0100

    Added webpage button

--- app/src/main/java/com/example/tomislav/marvelheros/View/ui/HeroesFragment.kt
+++ app/src/main/java/com/example/tomislav/marvelheros/View/ui/HeroesFragment.kt
@@ -1,6 +1,9 @@
 package com.example.tomislav.marvelheros.View.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,7 +22,8 @@ class HeroesFragment() : DaggerFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
          super.onCreateView(inflater, container, savedInstanceState)
 
-        return inflater.inflate( R.layout.fragment_heroes, container, false)
+        return inflater.inflate(R.layout.fragment_heroes, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -32,7 +36,15 @@ class HeroesFragment() : DaggerFragment() {
         }
         hero_item_name.text=hero.name;
         Glide.with(this).load(hero.thumbnail.getImageUrl()).into(hero_item_image)
+
+        val fab = fab
+        fab.setOnClickListener({
+            val intent = Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(hero.urls.get(1).url))
+            startActivity(intent);
+        })
         }
+
 
 
     }
